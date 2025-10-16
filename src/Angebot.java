250534030e74d3df714de39a72138fb34b35a749
Klemens <klkuen@tsn.at>
2025-10-16 10:25:05 +0200

    in beiden Klassen getter-und setter

--- src/Angebot.java
+++ src/Angebot.java
@@ -12,5 +12,27 @@ public class Angebot {
         this.flugnummer = flugnummer;
     }
 
+    public double getRegulärpreis() {
+        return regulärpreis;
+    }
 
+    public void setRegulärpreis(double regulärpreis) {
+        this.regulärpreis = regulärpreis;
+    }
+
+    public GregorianCalendar getFlugdatum() {
+        return flugdatum;
+    }
+
+    public void setFlugdatum(GregorianCalendar flugdatum) {
+        this.flugdatum = flugdatum;
+    }
+
+    public String getFlugnummer() {
+        return flugnummer;
+    }
+
+    public void setFlugnummer(String flugnummer) {
+        this.flugnummer = flugnummer;
+    }
 }
--- src/Rabattstrategie.java
+++ src/Rabattstrategie.java
@@ -6,4 +6,11 @@ public abstract class Rabattstrategie {
         this.bezeichnung = bezeichnung;
     }
 
+    public String getBezeichnung() {
+        return bezeichnung;
+    }
+
+    public void setBezeichnung(String bezeichnung) {
+        this.bezeichnung = bezeichnung;
+    }
 }
